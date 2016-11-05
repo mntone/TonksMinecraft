874bbab535f6adca93ad98f159359fed8d382f29
mntone <sd8@live.jp>
2016-11-05 12:46:40 +0900

    SNAPSHOT-20161105-124640

--- mods/write_mods.sh
+++ mods/write_mods.sh
@@ -2,7 +2,7 @@
 FILENAME=mods_list.txt
 rm $FILENAME
 for file in *; do
-	if [ $file != "t.sh" ] && [ $file != "1.7.10" ]; then
+	if [ $file != "write_mods.sh" ] && [ $file != "1.7.10" ]; then
     	echo "${file}" >> $FILENAME
 	fi
 done
